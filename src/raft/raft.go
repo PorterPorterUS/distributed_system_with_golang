cd71321ceb7b32901789c32c7e3fe4d0b71d106b
PorterPorterUS <xji6@ur.rochester.edu>
2020-05-30 05:41:22 -0700

    raft2A- modify getState and finish raft2A

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -85,10 +85,14 @@ type Raft struct {
 // return currentTerm and whether this server
 // believes it is the leader.
 func (rf *Raft) GetState() (int, bool) {
-
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
 	var term int
 	var isleader bool
 	// Your code here (2A).
+	term = rf.currentTerm
+	isleader = rf.state == Leader
+
 	return term, isleader
 }
 
@@ -126,6 +130,16 @@ func (rf *Raft) readPersist(data []byte) {
 	if data == nil || len(data) < 1 { // bootstrap without any state?
 		return
 	}
+	r := bytes.NewBuffer(data)
+	d := labgob.NewEncoder(r)
+	var currentTerm, votedFor int
+	if d.Encode(currentTerm) != nil || d.Encode(votedFor) != nil {
+		DPrintf("%v fails to recover from persist", rf)
+		return
+	}
+	rf.currentTerm = currentTerm
+	rf.votedFor = votedFor
+
 	// Your code here (2C).
 	// Example:
 	// r := bytes.NewBuffer(data)
