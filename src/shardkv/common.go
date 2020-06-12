99e430860a96b0ce16ca915b5dc1b92433750af4
PorterPorterUS <xji6@ur.rochester.edu>
2020-06-12 14:29:10 -0700

    lab4b-common.go

--- src/shardkv/common.go
+++ src/shardkv/common.go
@@ -8,6 +8,11 @@ package shardkv
 //
 // You will have to modify these definitions.
 //
+import (
+	"../shardmaster"
+	"log"
+	"math/rand"
+)
 
 const (
 	OK             = "OK"
@@ -18,6 +23,11 @@ const (
 
 type Err string
 
+type BaseArgs struct {
+	ClientId string
+	Ticket   string
+}
+
 // Put or Append
 type PutAppendArgs struct {
 	// You'll have to add definitions here.
@@ -27,18 +37,76 @@ type PutAppendArgs struct {
 	// You'll have to add definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	BaseArgs
 }
 
 type PutAppendReply struct {
-	Err Err
+	Err         Err
+	WrongLeader bool
 }
 
 type GetArgs struct {
 	Key string
 	// You'll have to add definitions here.
+	BaseArgs
 }
 
 type GetReply struct {
-	Err   Err
-	Value string
+	Err         Err
+	Value       string
+	WrongLeader bool
+}
+
+type DeleteShardsArgs struct {
+	//Key string
+	//// You'll have to add definitions here.
+	//BaseArgs
+	Num     int
+	ShardId int // 需要被删除的shard id
+}
+
+type DeleteShardsReply struct {
+	Err Err
+	//Value string
+	WrongLeader bool
+}
+
+type MigrateShardsArgs struct {
+	SourceGid int
+	Num       int
+	ShardsId  []int
+	//Key string
+	//// You'll have to add definitions here.
+	//BaseArgs
+}
+
+type MigrateShardsReply struct {
+	//Err   Err
+	//Value string
+	//Wrongleader bool
+	WrongLeader bool
+	Err         Err
+	Ack         map[string]string
+	Msg         string
+	Data        [shardmaster.NShards]map[string]string
+}
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func UUID() string {
+	n := 6
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+const Debug = 0
+
+func DPrintf(format string, a ...interface{}) {
+	if Debug > 0 {
+		log.Printf("%%%%%%"+format, a...)
+	}
+	return
 }
--- src/shardmaster/client.go
+++ src/shardmaster/client.go
@@ -6,9 +6,8 @@ package shardmaster
 
 import "../labrpc"
 import "time"
-import "math/rand"
-
-//import "math/big"
+import "crypto/rand"
+import "math/big"
 
 type Clerk struct {
 	servers []*labrpc.ClientEnd
@@ -17,35 +16,32 @@ type Clerk struct {
 	leader int
 }
 
-//func nrand() int64 {
-//	max := big.NewInt(int64(1) << 62)
-//	bigx, _ := rand.Int(rand.Reader, max)
-//	x := bigx.Int64()
-//	return x
-//}
+func nrand() int64 {
+	max := big.NewInt(int64(1) << 62)
+	bigx, _ := rand.Int(rand.Reader, max)
+	x := bigx.Int64()
+	return x
+}
 
 func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
-	// Your code here.
 	ck.Id = UUID()
-
+	// Your code here.
 	return ck
 }
 
 func (ck *Clerk) Query(num int) Config {
-	//args := &QueryArgs{}
-	// Your code here.
-	args := QueryArgs{BaseArgs{Ticket: UUID(), ClientId: ck.Id}, num}
-	args.Num = num
+	DPrintf("[Client %s]ShardMaster.Querying #%d...", ck.Id, num)
+	args := &QueryArgs{Num: num, BaseArgs: BaseArgs{ClientId: ck.Id, Ticket: UUID()}}
 	index := ck.leader
 	for {
 		currentLeader := index % len(ck.servers)
-		src := ck.servers[currentLeader]
+		srv := ck.servers[currentLeader]
 		index++
 
-		var reply QueryReply
-		ok := src.Call("ShardMaster.Query", &args, &reply)
+		reply := QueryReply{}
+		ok := srv.Call("ShardMaster.Query", args, &reply)
 		if !ok {
 			DPrintf("[Client %s]ShardMaster.Query #%d failed, request failed", ck.Id, num)
 			time.Sleep(100 * time.Millisecond)
@@ -55,7 +51,7 @@ func (ck *Clerk) Query(num int) Config {
 			time.Sleep(100 * time.Millisecond)
 			continue
 		} else if reply.Err != "" {
-			DPrintf("[Client %s]ShardMaster.Query #%d failed, request failed", ck.Id, num)
+			DPrintf("[Client %s]ShardMaster.Query #%d failed, %s", ck.Id, num, reply.Err)
 			time.Sleep(100 * time.Millisecond)
 			continue
 		}
@@ -66,34 +62,33 @@ func (ck *Clerk) Query(num int) Config {
 }
 
 func (ck *Clerk) Join(servers map[int][]string) {
-	//args := &QueryArgs{}
-	// Your code here.
-	args := JoinArgs{BaseArgs{Ticket: UUID(), ClientId: ck.Id}, servers}
-	//args.Num = num
+	DPrintf("[Client %s] ShardMaster.Joining servers %+v", ck.Id, servers)
+	args := &JoinArgs{Servers: servers, BaseArgs: BaseArgs{ClientId: ck.Id, Ticket: UUID()}}
 	index := ck.leader
 	for {
 		currentLeader := index % len(ck.servers)
-		src := ck.servers[currentLeader]
+		srv := ck.servers[currentLeader]
 		index++
 
-		var reply JoinReply
-		ok := src.Call("ShardMaster.Join", &args, &reply)
+		reply := JoinReply{}
+		ok := srv.Call("ShardMaster.Join", args, &reply)
 		if !ok {
-			DPrintf("[Client %s]ShardMaster.Query #%d failed, request failed", ck.Id, servers)
+			DPrintf("[Client %s] ShardMaster.Join servers %+v failed, request failed", ck.Id, servers)
 			time.Sleep(100 * time.Millisecond)
 			continue
 		} else if reply.WrongLeader {
-			DPrintf("[Client %s]ShardMaster.Query #%d failed, wrong leader", ck.Id, servers)
+			DPrintf("[Client %s] ShardMaster.Join servers %+v failed, wrong leader", ck.Id, servers)
 			time.Sleep(100 * time.Millisecond)
 			continue
 		} else if reply.Err != "" {
-			DPrintf("[Client %s]ShardMaster.Query #%d failed, request failed", ck.Id, servers)
+			DPrintf("[Client %s] ShardMaster.Join servers %+v failed, %s", ck.Id, servers, reply.Err)
 			time.Sleep(100 * time.Millisecond)
 			continue
 		}
-		DPrintf("[Client %s]ShardMaster.Query #%d successfully, result is %+v", ck.Id, servers)
+
+		DPrintf("[Client %s] ShardMaster.Join servers %+v successfully", ck.Id, servers)
 		ck.leader = currentLeader
-		//return reply.Config
+		return
 	}
 }
 
@@ -131,20 +126,34 @@ func (ck *Clerk) Leave(gids []int) {
 }
 
 func (ck *Clerk) Move(shard int, gid int) {
-	args := &MoveArgs{}
-	// Your code here.
-	args.Shard = shard
-	args.GID = gid
+	DPrintf("[Client %s]ShardMaster.Moving the shard %d to group %d", ck.Id, shard, gid)
+	args := &MoveArgs{MoveShardArgs: MoveShardArgs{Shard: shard, GID: gid}, BaseArgs: BaseArgs{ClientId: ck.Id, Ticket: UUID()}}
 
+	index := ck.leader
 	for {
+		currentLeader := index % len(ck.servers)
+		srv := ck.servers[currentLeader]
+		index++
 		// try each known server.
-		for _, srv := range ck.servers {
-			var reply MoveReply
-			ok := srv.Call("ShardMaster.Move", args, &reply)
-			if ok && reply.WrongLeader == false {
-				return
-			}
+		reply := MoveReply{}
+		ok := srv.Call("ShardMaster.Move", args, &reply)
+
+		if !ok {
+			DPrintf("[Client %s]ShardMaster.Move the shard %d to group %d failed, request failed", ck.Id, shard, gid)
+			time.Sleep(100 * time.Millisecond)
+			continue
+		} else if reply.WrongLeader {
+			DPrintf("[Client %s]ShardMaster.Move the shard %d to group %d failed, wrong leader", ck.Id, shard, gid)
+			time.Sleep(100 * time.Millisecond)
+			continue
+		} else if reply.Err != "" {
+			DPrintf("[Client %s]ShardMaster.Move the shard %d to group %d failed, %s", ck.Id, shard, gid, reply.Err)
+			time.Sleep(100 * time.Millisecond)
+			continue
 		}
-		time.Sleep(100 * time.Millisecond)
+
+		DPrintf("[Client %s]ShardMaster.Move the shard %d to group %v successfully", ck.Id, shard, gid, reply.Err)
+		ck.leader = currentLeader
+		return
 	}
 }
